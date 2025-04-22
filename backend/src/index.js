de03b8986ecdeeeb2145efbede1fbd1204d905f6
karan5772 <karankumar8239@gmail.com>
2025-04-22 22:07:31 +0530

    Add route for welcome message in "/" (root route)

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+
 dotenv.config();
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to leetLab 🔥");
+});
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
