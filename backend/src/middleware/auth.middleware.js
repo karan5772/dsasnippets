ad66f67d10e143ac945f5ba043ef0511f0e994ba
karan5772 <karankumar8239@gmail.com>
2025-05-10 11:09:07 +0530

    Created checkAdmin middleware

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -51,3 +51,30 @@ export const authMwiddleware = async (req, res, next) => {
     });
   }
 };
+
+export const checkAdmin = async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+    const user = await db.user.findUnique({
+      where: {
+        id: userId,
+      },
+      select: {
+        role: true,
+      },
+    });
+
+    if (!user || user.role != ADMIN) {
+      return res.status(403).json({
+        success: false,
+        message: "Access Denied - Admins only",
+      });
+    }
+  } catch (error) {
+    console.log("Error checking Admin role:", error);
+    return res.status(500).json({
+      message: "Error checking Admin role",
+      success: false,
+    });
+  }
+};
