e27eeb1e2e25d727c9cbe6c649d1c7c6c1f8c1d6
karan5772 <karankumar8239@gmail.com>
2025-05-12 16:33:29 +0530

    Wrote judge0 utilities

---
+++ backend/src/libs/judge0.lib.js
@@ -0,0 +1,46 @@
+import axios from "axios";
+export const getLangaugeId = function (langauge) {
+  const langaugeMap = {
+    JAVA: 62,
+    PYTHON: 71,
+    JAAVASCRIPT: 63,
+  };
+
+  return langaugeMap[langauge.toUpperCase()];
+};
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+export const poolBatchResults = async (tokens) => {
+  while (true) {
+    const { data } = await axios.get(
+      `${(process.env, JUDGE0_API_URL)}/submissions/batch`,
+      {
+        params: {
+          tokens: tokens.join(","),
+          base64_encoded: false,
+        },
+      }
+    );
+
+    const results = data.submissions;
+
+    const isAllDone = results.every((r) => r.status.id > 2); // it only returns true if the status if of the submition is more then 2
+
+    if (isAllDone) {
+      return results;
+    }
+
+    await sleep(1000); // if status id is less then 2 then it will hit the judge0 again after 1 second
+  }
+};
+
+export const submitBatch = async (submissions) => {
+  const { data } = await axios.post(
+    `${process.env.JUDGE0_API_URL}/submissions/batch?base64_encoded=false`,
+    {
+      submissions,
+    }
+  );
+  return data;
+};
