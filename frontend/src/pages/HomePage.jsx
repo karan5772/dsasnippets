b7a536f6803bc7f8510473043197d9a4e40e16bb
karan5772 <karankumar8239@gmail.com>
2025-05-27 13:55:16 +0530

    Added navbar and implemented layout

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="min-h-screen flex flex-col items-center mt-14 px-4">
+      <div className="absolute top-16 left-0 w-1/3 h-1/3 bg-primary opacity-30 blur-3xl rounded-md bottom-9"></div>
+      <h1 className="text-4xl font-extrabold z-10 text-center">
+        Welcome to <span className="text-primary">DSASNIPPETS</span>
+      </h1>
+
+      <p className="mt-4 text-center text-lg font-semibold text-gray-300 dark:text-gray-300 z-10">
+        The next-generation coding platform where{" "}
+        <span className="text-blue-500 font-semibold">
+          creators build problems
+        </span>{" "}
+        and{" "}
+        <span className="text-cyan-400 font-semibold">coders solve them</span>.
+        <br />
+        Create questions manually or with AI, sell premium playlists, and
+        execute code in real-time.
+      </p>
+    </div>
+  );
 };
 
 export default HomePage;
